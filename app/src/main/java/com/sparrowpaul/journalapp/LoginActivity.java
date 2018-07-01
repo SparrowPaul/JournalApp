63f251461769cea544361fbd76ffef3a59d89ae6
Sparrow Paul <sparrowpaul2@gmail.com>
2018-07-01 05:52:55 +0000

    Initial Commit

--- app/src/main/java/com/sparrowpaul/journalapp/LoginActivity.java
+++ app/src/main/java/com/sparrowpaul/journalapp/LoginActivity.java
@@ -38,11 +38,6 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //enabling fullscreen
-
-//        requestWindowFeature(Window.FEATURE_NO_TITLE);
-//        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-//                WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
 
 
